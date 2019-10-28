6297b18e25b051adf42b0253335b57918369e36a
Amir Ohadi <amirmohammadohadi@gmail.com>
2019-10-28 18:47:48 +0330

    project structures created. headers added.

---
+++ include/canvas.h
@@ -0,0 +1,11 @@
+#ifndef CANVAS_H
+#define CANVAS_H
+#include "polygon.h"
+#include <vector>
+class Canvas{
+private:
+    // std::vector<polygon> polygons;
+public: 
+    // public bool doesConflict();
+};
+#endif
